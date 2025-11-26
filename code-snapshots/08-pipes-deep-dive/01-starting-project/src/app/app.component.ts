aa772795ddda7180f08d5b6a305ea0c8aa28c6db
Ness Gira <nessgira1@gmail.com>
2025-11-26 19:00:19 +0100

    pipes

--- code-snapshots/08-pipes-deep-dive/01-starting-project/src/app/app.component.ts
+++ code-snapshots/08-pipes-deep-dive/01-starting-project/src/app/app.component.ts
@@ -1,13 +1,22 @@
 import { Component } from '@angular/core';
+import {DatePipe, DecimalPipe} from "@angular/common";
+import {TemperaturePipe} from "./temperature.pipe";
+import {SortPipe} from "./sort.pipe";
 
 @Component({
   selector: 'app-root',
   standalone: true,
   templateUrl: './app.component.html',
+  imports: [
+    DatePipe,
+    DecimalPipe,
+    TemperaturePipe,
+    SortPipe
+  ]
 })
 export class AppComponent {
   currentDate = new Date();
-  currentTemperaturs = {
+  currentTemperatures = {
     berlin: 4.2749812,
     newYork: 18.1214,
     paris: 72.1209001,
@@ -18,7 +27,13 @@ export class AppComponent {
     25, 37, 19, -4, 28, 21, 19, 28, 33, 31, 9, 11, 5, -12, -5,
   ];
 
+  constructor() {
+    this.historicTemperatures.sort((a,b) => a > b ? 1 : -1);
+  }
+
   onReset(index: number) {
-    this.historicTemperatures[index] = 18;
+    const newTemps = [...this.historicTemperatures] ;
+    newTemps[index] = 18;
+    this.historicTemperatures = newTemps;
   }
 }
