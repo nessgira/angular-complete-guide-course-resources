7af88630e874456b026ef6fb9dba0c7ec380a70b
Ness Gira <nessgira1@gmail.com>
2025-12-01 11:57:21 +0100

    delete request + interceptor fn

--- code-snapshots/12-http/01-starting-project/src/app/places/places.service.ts
+++ code-snapshots/12-http/01-starting-project/src/app/places/places.service.ts
@@ -1,13 +1,15 @@
 import {inject, Injectable, signal} from '@angular/core';
 
 import {Place} from './place.model';
-import {catchError, map, throwError} from "rxjs";
+import {catchError, map, tap, throwError} from "rxjs";
 import {HttpClient} from "@angular/common/http";
+import {ErrorService} from "../shared/error.service";
 
 @Injectable({
   providedIn: 'root',
 })
 export class PlacesService {
+  private errorService = inject(ErrorService);
   private httpClient = inject(HttpClient);
   private userPlaces = signal<Place[]>([]);
 
@@ -22,16 +24,46 @@ export class PlacesService {
   loadUserPlaces() {
     return this.fetchPlaces(
       'http://localhost:3000/user-places',
-      "Something went wrong fetching your favorite places. Please try again later.");
+      "Something went wrong fetching your favorite places. Please try again later.").
+    pipe(tap({
+      next: (userPlaces) => this.userPlaces.set(userPlaces),
+    }));
   }
 
-  addPlaceToUserPlaces(placeId: string) {
+  addPlaceToUserPlaces(place: Place) {
+    const prevPlaces = this.userPlaces();
+
+    if (!prevPlaces.some((p) => p.id === place.id)) {
+      this.userPlaces.set([...prevPlaces, place]);
+    }
+
     return this.httpClient.put('http://localhost:3000/user-places', {
-      placeId: placeId
-    })
+      placeId: place.id
+    }).pipe(
+      catchError(error => {
+        this.userPlaces.set(prevPlaces);
+        this.errorService.showError("Failed to store selected place.");
+        return throwError(() => new Error("Failed to store selected place."))
+      })
+    );
   }
 
   removeUserPlace(place: Place) {
+    const prevPlaces = this.userPlaces();
+
+    if (prevPlaces.some((p) => p.id === place.id)) {
+      this.userPlaces.set(prevPlaces.filter(p => p.id !== place.id));
+    }
+
+    return this.httpClient.delete('http://localhost:3000/user-places/' + place.id)
+      .pipe(
+        catchError((error) => {
+          this.userPlaces.set(prevPlaces);
+          this.errorService.showError("Failed to remove selected place.");
+          return throwError(() => new Error("Failed to remove selected place."));
+        })
+      )
+
   }
 
   private fetchPlaces(url: string, errorMessage: string) {
--- code-snapshots/12-http/01-starting-project/src/app/places/user-places/user-places.component.ts
+++ code-snapshots/12-http/01-starting-project/src/app/places/user-places/user-places.component.ts
@@ -15,18 +15,15 @@ import {PlacesService} from "../places.service";
   imports: [PlacesContainerComponent, PlacesComponent],
 })
 export class UserPlacesComponent implements OnInit {
-  places = signal<Place[] | undefined>(undefined);
   isFetching = signal(false);
   error = signal('');
   private placesService = inject(PlacesService);
   private destroyRef = inject(DestroyRef);
+  places = this.placesService.loadedUserPlaces;
 
   ngOnInit() {
     this.isFetching.set(true);
     const subscription = this.placesService.loadUserPlaces().subscribe({
-        next: (places) => {
-          this.places.set(places);
-        },
         complete: () => {
           this.isFetching.set(false);
         },
@@ -38,6 +35,14 @@ export class UserPlacesComponent implements OnInit {
 
     this.destroyRef.onDestroy(() => {
       subscription.unsubscribe();
-    })
+    });
+  }
+
+  onRemovePlace(place: Place) {
+    const subscription = this.placesService.removeUserPlace(place).subscribe();
+
+    this.destroyRef.onDestroy(() => {
+      subscription.unsubscribe();
+    });
   }
 }
