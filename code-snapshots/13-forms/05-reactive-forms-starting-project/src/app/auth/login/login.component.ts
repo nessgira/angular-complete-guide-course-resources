a2200a3a02119a57fce23a7696c95a3b9afd7598
Ness Gira <nessgira1@gmail.com>
2025-12-04 18:38:09 +0100

    reactive forms - final part

--- code-snapshots/13-forms/05-reactive-forms-starting-project/src/app/auth/login/login.component.ts
+++ code-snapshots/13-forms/05-reactive-forms-starting-project/src/app/auth/login/login.component.ts
@@ -16,6 +16,14 @@ function emailIsUnique(control: AbstractControl) {
   return of({notUnique: true});
 }
 
+let initialEmailValue = '';
+const savedForm = window.localStorage.getItem(('saved-login-form'));
+
+if (savedForm) {
+  const loadedForm = JSON.parse(savedForm);
+  initialEmailValue = loadedForm.email;
+}
+
 @Component({
   selector: 'app-login',
   standalone: true,
@@ -26,9 +34,8 @@ function emailIsUnique(control: AbstractControl) {
 export class LoginComponent implements OnInit {
   private destroyRef = inject(DestroyRef);
 
-
   form = new FormGroup({
-    email: new FormControl('', {
+    email: new FormControl(initialEmailValue, {
       validators: [ Validators.email, Validators.required ],
       asyncValidators : [ emailIsUnique ]
     }),
@@ -54,15 +61,6 @@ export class LoginComponent implements OnInit {
   }
 
   ngOnInit() {
-    const savedForm = window.localStorage.getItem(('saved-login-form'));
-
-    if (savedForm) {
-      const loadedForm = JSON.parse(savedForm);
-      this.form.patchValue({
-        email: loadedForm.email,
-      })
-    }
-
     const subscription = this.form.valueChanges.pipe(debounceTime(500)).subscribe({
       next: (value) => {
         window.localStorage.setItem(
