769d66f1f27fa32d703bd5bfb621a985ac392e1a
Ness Gira <nessgira1@gmail.com>
2025-12-07 18:55:45 +0100

    routes - not found page

--- code-snapshots/14-routing/01-starting-project/src/app/app.routes.ts
+++ code-snapshots/14-routing/01-starting-project/src/app/app.routes.ts
@@ -3,6 +3,7 @@ import {TasksComponent} from "./tasks/tasks.component";
 import {NoTaskComponent} from "./tasks/no-task/no-task.component";
 import {UserTasksComponent} from "./users/user-tasks/user-tasks.component";
 import {NewTaskComponent} from "./tasks/new-task/new-task.component";
+import {NotFoundComponent} from "./not-found/not-found.component";
 
 export const routes: Routes = [
   {
@@ -20,6 +21,10 @@ export const routes: Routes = [
       {
         path: 'tasks/new',
         component: NewTaskComponent
+      },
+      {
+        path: '**',
+        component: NotFoundComponent
       }
     ]
   },
