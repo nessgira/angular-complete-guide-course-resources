c8d54bff075ff2d47206d7f547eb9a7a93f3f8ce
Ness Gira <nessgira1@gmail.com>
2025-12-07 18:09:23 +0100

    routes - dynamic routes, nested routes

--- code-snapshots/14-routing/01-starting-project/src/app/users/user-tasks/user-tasks.component.ts
+++ code-snapshots/14-routing/01-starting-project/src/app/users/user-tasks/user-tasks.component.ts
@@ -1,9 +1,23 @@
-import { Component } from '@angular/core';
+import {Component, computed, inject, input, OnInit} from '@angular/core';
+import {UsersService} from "../users.service";
+import {RouterLink, RouterOutlet} from "@angular/router";
 
 @Component({
   selector: 'app-user-tasks',
   standalone: true,
   templateUrl: './user-tasks.component.html',
   styleUrl: './user-tasks.component.css',
+  imports: [
+    RouterOutlet,
+    RouterLink
+  ]
 })
-export class UserTasksComponent {}
+export class UserTasksComponent {
+  userId = input.required<string>();
+  private usersService = inject(UsersService);
+
+  userName = computed(
+    () => this.usersService.users
+      .find(u => u.id === this.userId())?.name);
+
+}
